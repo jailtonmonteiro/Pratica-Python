12e18679f4bd1299312e72856e00aa41a9194e85
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2025-06-25 16:15:02 -0300

    first commit

---
+++ Estrutura de Dados/atletas.py
@@ -0,0 +1,27 @@
+atletas = [
+    ["Maria Silva", 1.75, 65],
+    ["João Santos", 1.80, 72],
+    ["Ana Pereira", 1.68, 58],
+    ["Pedro Oliveira", 1.92, 85],
+    ["Carlos Lima", 1.85, 78],
+    ["Beatriz Souza", 1.70, 60],
+    ["Fernanda Costa", 1.62, 55],
+    ["Lucas Almeida", 1.88, 82],
+    ["Rafaela Gomes", 1.74, 63],
+    ["Gustavo Ferreira", 1.90, 88],
+    ["Larissa Rocha", 1.66, 57],
+    ["Henrique Nunes", 1.83, 76],
+    ["Juliana Martins", 1.72, 59],
+    ["Ricardo Carvalho", 1.86, 80],
+    ["Sofia Alves", 1.64, 54],
+    ["Matheus Ribeiro", 1.89, 84],
+    ["Camila Duarte", 1.69, 61],
+    ["Gabriel Monteiro", 1.77, 73],
+    ["Eduarda Farias", 1.71, 62],
+    ["Thiago Mendes", 1.84, 79],
+]
+
+for atleta in atletas:
+    print(f'Nome: {atleta[0]}')
+    print(f'Altura: {atleta[1]}m')
+    print(f'Peso: {atleta[2]} Kg\n')
